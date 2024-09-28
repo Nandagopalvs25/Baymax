d906d53de30f1d21e24ab1cef6363360892f4db0
Nandagopalvs25 <nandagopalvs12@gmail.com>
2024-09-29 01:30:18 +0530

    Adding context for chat

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Record,Relation
+from .models import Record,Relation,Message
 # Register your models here.
 
 
 admin.site.register(Record)
 admin.site.register(Relation)
+admin.site.register(Message)
--- backend/api/models.py
+++ backend/api/models.py
@@ -37,3 +37,16 @@ class Relation(models.Model):
    def __str__(self):
         return self.user.username
     
+    
+class Message(models.Model):
+        user=models.ForeignKey(User, on_delete=models.CASCADE)
+        content=models.TextField()
+        date=models.DateField(auto_now_add=True)
+        
+        def __str__(self):
+          return self.user.username
+
+        
+
+    
+    
