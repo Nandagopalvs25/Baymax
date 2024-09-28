d638298bf8a5c99d7ecf69cbea1c6885d03f9a22
Nandagopalvs25 <nandagopalvs12@gmail.com>
2024-09-28 16:34:22 +0530

    Csrf fix

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -90,7 +90,16 @@ AUTHENTICATION_BACKENDS = [
 ]
 WSGI_APPLICATION = 'backend.wsgi.application'
 
-
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ],
+      'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ]
+}
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
